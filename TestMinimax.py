b725119ad63d95bbbc032e56c92fb04bf8c39ea7
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-12-02 00:24:59 +0200

    add some modifications

--- TestMinimax.py
+++ TestMinimax.py
@@ -1,7 +1,7 @@
 import score
 
 connect4_board = [
-    ['0', '0', '0', '0', '0', '0', '0'] ,
+['0', '0', '0', '0', '0', '0', '0'] ,
 ['0', '0', '0', '0', '0', '0', '0'] ,
 ['0', '0', '0', '0', '0', '0', '0'] ,
 ['0', '0', '0', '0', '0', '0', '0'] ,
@@ -9,16 +9,17 @@ connect4_board = [
 ['0', '0', '0', '0', '0', '0', '0']
 ]
 
+#game_counter=0
 grid='0'*42
-limit=10
+limit=8
 next_play=[5,5,5,5,5,5,5]
-
+column_score=[1,2,3,4,3,2,1]
 def make_state(grid,i,p):
 
     index=next_play[i]*7+i
 
     state=grid[:index]+p+grid[index+1:]
-    # print(state)
+    #print(state)
     return state
 
 def maximize(counter,state):     # computer
@@ -29,21 +30,23 @@ def maximize(counter,state):     # computer
 
 
     max_column = -1
+    max_column_score=0
     for i in range(7):
-        if(state[next_play[i]*7+i]=='0'):
+        if(next_play[i]!=-1 and column_score[i]>max_column_score):
+            max_column_score=column_score[i]
             max_column=i
-            break
-    #print(max_column)
+
     maxEvaluation = -1000000  # large negative
 
     for i in range(7):
        if next_play[i]!=-1:
-            heuristic=minimize(counter+1,make_state(state,i,'1'))
-
+            x=make_state(state,i,'1')
+            next_play[i]-=1
+            heuristic=minimize(counter+1,x)
+            next_play[i]+=1
             if heuristic[1] > maxEvaluation:
                 max_column=i
                 maxEvaluation=heuristic[1]
-
     return max_column,maxEvaluation
 
 
@@ -53,16 +56,19 @@ def minimize(counter,state):     # human
        return None,score.get_score(state)
 
     min_column = -1
+    col_score=0
     min_evaluation=1000000
     for i in range(7):           #set to first un inhabitant position
-        if (state[next_play[i]*7+i]=='0'):
+        if (state[next_play[i]*7+i]=='0' and col_score<column_score[i]):
+            col_score=column_score[i]
             min_column = i
-            break
-    #print(min_column)
-    for i in range(7):
 
+    for i in range(7):
        if next_play[i]!=-1 :
-            heuristic = maximize(counter+1,make_state(state,i,'2'))
+            x=make_state(state,i,'2')
+            next_play[i]-=1
+            heuristic = maximize(counter+1,x)
+            next_play[i]+=1
             if heuristic[1] < min_evaluation:
                 min_column = i
                 min_evaluation = heuristic[1]
@@ -74,22 +80,25 @@ def minimize(counter,state):     # human
 
 while sum(next_play)!=-7:
     print("computer turn:")
-    mmm=maximize(1,grid)
-    col=mmm[0]
+    mmm = maximize(1, grid)
+    col = mmm[0]
+    print("I  choose column : " + str(col))
     print("\n My heuristic is \n")
     print(mmm[1])
-    connect4_board[next_play[col]][col]='1'
-    index=next_play[col]*7+col
-    #grid[next_play[col]*7+col]='1'
-    grid=grid[:index]+'1'+grid[index+1:]
-    next_play[col]-=1
+    connect4_board[next_play[col]][col] = '1'
+    index = next_play[col] * 7 + col
+    grid = grid[:index] + '1' + grid[index + 1:]
+    next_play[col] -= 1
 
     for i in range(6):
         print(connect4_board[i])
 
-    x=input("enter a column :")
-    connect4_board[next_play[int(x)]][int(x)]='2'
-    next_play[int(x)]-=1
+    x = input("enter a column :")
+    connect4_board[next_play[int(x)]][int(x)] = '2'
+    index = next_play[int(x)] * 7 + int(x)
+    grid = grid[:index] + '2' + grid[index + 1:]
+    next_play[int(x)] -= 1
+
     for i in range(6):
         print(connect4_board[i])
 
--- score.py
+++ score.py
@@ -1,25 +1,61 @@
+
+def string_index(index):
+    return index//7,index% 7
+
+def grid_index(i,j):
+    return i*7+j
+def valid(i,j):
+    return not (i>5 or i<0 or j>6 or j<0)
+def shift_to_grid(shift):
+    if(shift==1):
+        return 0,1
+    elif shift==7:
+        return 1,0
+    elif shift==8:
+        return 1,1
+    elif shift==6:
+        return 1,-1
+
 def score(grid,index,p, shift,player):
+
+    g_index= string_index(index)
+    shift_margin=shift_to_grid(shift)
     counter=0
     for i in range(4):
-        if(index+i*shift<=41 and grid[index+i*shift]==player):
+        new_index=[g_index[0]+shift_margin[0]*i,g_index[1]+shift_margin[1]*i]
+        if(valid(new_index[0], new_index[1]) and grid[grid_index(new_index[0], new_index[1])]==player):
+            #print(new_index)
             counter+=1
         else:
             break
+
+    # print(counter)
+    # print("\n")
+    minus_shift=[g_index[0]-shift_margin[0],g_index[1]-shift_margin[1]]
+    three_shift=[g_index[0] + 3 * shift_margin[0], g_index[1] + 3 * shift_margin[1]]
+    two_shift=[g_index[0] + 2 * shift_margin[0], g_index[1] + 2 * shift_margin[1]]
     if(counter==4):
         p[0]+=1
-    elif(counter==3 and grid[index-shift]!=player) and (grid[index-shift]=='0' or(index+3*shift<41 and grid[index+3*shift]=='0')):
+    elif (counter == 3) and (((valid(minus_shift[0], minus_shift[1]))and grid[grid_index(minus_shift[0], minus_shift[1])] == '0' and (not valid(minus_shift[0]+1,minus_shift[1]) or grid[grid_index(minus_shift[0]+1,minus_shift[1])]!='0'))
+          or ((valid(three_shift[0], three_shift[1]) and grid[grid_index(three_shift[0], three_shift[1])] == '0') and (not valid(three_shift[0] + 1, three_shift[1]) or grid[grid_index(three_shift[0] + 1, three_shift[1])] != '0'))):
         p[1]+=1
-    elif(counter==2 and grid[index-shift]!=player) and (grid[index-shift]=='0' or(index+2*shift<41 and grid[index+2*shift]=='0')):
+    elif (counter == 2) and (((valid(minus_shift[0], minus_shift[1])) and grid[grid_index(minus_shift[0], minus_shift[1])] == '0' and (not valid(minus_shift[0] + 1, minus_shift[1]) or grid[grid_index(minus_shift[0] + 1, minus_shift[1])] != '0'))
+          or ((valid(two_shift[0], two_shift[1]) and grid[grid_index(two_shift[0], two_shift[1])] == '0') and (not valid(two_shift[0] + 1, two_shift[1]) or grid[grid_index(two_shift[0] + 1, two_shift[1])] != '0'))):
         p[2]+=1
 
 
-
+    # print(p[0])
+    # print(p[1])
+    # print(p[2])
 
 def cell_score(grid, i,p):
-
+    #print("UP")
     score(grid, i, p,7,grid[i])  #up
-    score(grid, i, p, 1,  grid[i])  #left
+    #print("Right")
+    score(grid, i, p, 1,  grid[i])  #right
+    #print("Diagonal_Right")
     score(grid, i, p, 8, grid[i])  #diagonal_left
+    #print("Diagonal_Right")
     score(grid, i, p, 6,  grid[i])  #diagonal_right
 
 
@@ -31,12 +67,10 @@ def get_score(grid):
         cell_score(grid,i,p1)
        elif grid[i]=='2':
          cell_score(grid,i,p2)
-    x=[1000,800,100]
+    x=[10000,5000,1000]
     heuristic=0
     for i in range(3):
-       # print(p1[i]-p2[i])
        heuristic+=x[i]*(p1[i]-p2[i])
-
     return heuristic
 
 def MakeTest(Grid):
@@ -47,13 +81,14 @@ def MakeTest(Grid):
 
     return grid
 
-connect4_board = [
-    ['2', '1', '2', '2', '1', '2', '2'],
-    ['2', '1', '2', '1', '1', '2', '2'],
-    ['2', '2', '1', '1', '2', '1', '2'],
-    ['2', '1', '2', '1', '1', '1', '2'],
-    ['2', '1', '1', '1', '1', '1', '2'],
-    ['1', '2', '1', '2', '1', '1', '2']
-]
+# connect4_board = [
+#     ['2', '2', '1', '1', '1', '1', '2'],
+#     ['1', '1', '1', '1', '1', '1', '2'],
+#     ['1', '2', '2', '1', '2', '2', '1'],
+#     ['2', '1', '2', '2', '2', '1', '2'],
+#     ['2', '1', '2', '2', '1', '2', '2'],
+#     ['2', '1', '1', '1', '1', '2', '2']
+#
+# ]
 
-get_score(MakeTest(connect4_board))  # it must print 1 0 0   # it now prints 1 1 1
+# print(get_score(MakeTest(connect4_board)))  # it must print 1 0 0   # it now prints 1 1 1
