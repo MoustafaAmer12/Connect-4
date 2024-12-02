875f9363d51f05f346591b4aeab9b417ace047df
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-12-02 07:00:27 +0200

    Class heuristics

--- Agents/Heuristic.py
+++ Agents/Heuristic.py
@@ -1,6 +1,4 @@
 class Heuristic:
-    """_summary_
-    """
     def __init__(self, state):
         self.state = state
 
--- Agents/Solver.py
+++ Agents/Solver.py
@@ -1,7 +1,11 @@
 class Solver:
     def __init__(self, max_depth, heuristic):
-        pass
+        self.max_depth = max_depth
+        self.eval_heuristic = heuristic.eval
+        self.state = "0"*42
+        self.plays_av = [6] * 7
+        self.neighbours = [3, 2, 4, 1, 5, 0, 6]
 
     # To Be Overriden
-    def play(self, currentState):
+    def play(self, counter):
         pass
