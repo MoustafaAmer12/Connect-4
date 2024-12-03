b12ed24680d1f954b2c70a10af90ff5cca4498b4
Ahmed Mostafa <am1694553@gmail.com>
2024-12-03 06:31:36 +0200

    alphabeta with node mapping for using in tree representation graph

--- Agents/AlphaBeta.py
+++ Agents/AlphaBeta.py
@@ -2,6 +2,8 @@ from Agents.Heuristic_Lec import Heuristic_Lec
 import sys
 from datetime import datetime
 from Agents.Solver import Solver
+from Agents.Node import Node
+
 
 class AlphaBeta(Solver):
     count = 1
@@ -13,6 +15,8 @@ class AlphaBeta(Solver):
     def play(self, state):
         self.grid = state
         self.state = "".join(self.grid[i][j] for i in range(len(self.grid)) for j in range(len(self.grid[0])))
+        self.state_node_map = {}
+
 
         for j in range(7):
             self.plays_av[j] = 0
@@ -27,9 +31,18 @@ class AlphaBeta(Solver):
         return self.minmax(0, maximizer, self.state, self.plays_av[:])  # Pass a copy of plays_av
 
     def minmax(self, counter, maximizer, state, av_moves, alpha=-sys.maxsize, beta=sys.maxsize):
+        if(self.state_node_map.__contains__(state)):
+            return None, self.state_node_map[state][0], self.state_node_map[state][1]
+        
+        node=Node()
+        node.type = "MAX" if maximizer else "MIN"
+
         turn = sum(av_moves)
         if counter == self.max_depth or turn == 0:
-            return None, self.eval_heuristic(state)
+            node.value = self.eval_heuristic(state)
+            node.children=None
+            self.state_node_map[state]=[node.value, node]
+            return None, node.value, node
 
         player = self.p1 if turn % 2 == 0 else self.p2
         play_col = None
@@ -45,20 +58,23 @@ class AlphaBeta(Solver):
                 new_state = state[:i + 7 * av_moves[i]] + player + state[i + 7 * av_moves[i] + 1:]
 
                 # Recursively call minmax for the minimizer
-                _, score = self.minmax(counter + 1, False, new_state, av_moves[:], alpha, beta)
+                _, score, new_node = self.minmax(counter + 1, False, new_state, av_moves[:], alpha, beta)
+                node.children.append(new_node)
                 av_moves[i] += 1  # Restore move
 
                 if score > max_score:
                     max_score = score
                     play_col = i
+                    node.value = max_score
 
                 alpha = max(alpha, score)
                 if alpha >= beta:  # Prune remaining branches
                     break
 
+            self.state_node_map[state] = [node.value, node]
             print("Maximizer Score:", max_score)
             self.print_state(state)
-            return play_col, max_score
+            return play_col, node.value, node
 
         else:
             min_score = sys.maxsize
@@ -71,20 +87,23 @@ class AlphaBeta(Solver):
                 new_state = state[:i + 7 * av_moves[i]] + player + state[i + 7 * av_moves[i] + 1:]
 
                 # Recursively call minmax for the maximizer
-                _, score = self.minmax(counter + 1, True, new_state, av_moves[:], alpha, beta)
+                _, score, new_node = self.minmax(counter + 1, True, new_state, av_moves[:], alpha, beta)
+                node.children.append(new_node)
                 av_moves[i] += 1  # Restore move
 
                 if score < min_score:
                     min_score = score
                     play_col = i
+                    node.value = min_score
 
                 beta = min(beta, score)
                 if beta <= alpha:  # Prune remaining branches
                     break
 
+            self.state_node_map[state] = [node.value, node]
             print("Minimizer Score:", min_score)
             self.print_state(state)
-            return play_col, min_score
+            return play_col, node.value, node
 
     def print_state(self, state):
         print("State Count:", self.count)
@@ -105,6 +124,6 @@ if __name__ == "__main__":
             ['0', '0', '0', '0', '0', '0', '0'],
             ['0', '0', '0', '0', '0', '0', '0'],
             ['0', '0', '0', '0', '0', '0', '0']]
-    col, score = minmax.play(game)
+    col, score, node = minmax.play(game)
     print("Chosen Column:", col)
     print("Execution Time:", (datetime.now() - st_time).total_seconds())
